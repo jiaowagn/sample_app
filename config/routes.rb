814c11300fe53e1395965534ed76c95d34b4aa46
jiaowagn <wangjiaohere@gmail.com>
2018-03-18 15:53:47 +0800

    Add account activation

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,8 @@ Rails.application.routes.draw do
 
   delete '/logout', to: 'sessions#destroy'
 
+  resources :account_activations, only: [:edit]
+
   root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
